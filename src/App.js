d91aaed605e69fc7a6375573715ded79dd7a5269
Mihir Pathak <mihirpathak97@gmail.com>
2018-05-28 14:36:06 +0530

    use react-router to load views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import Home from './views/Home';
+
 import './App.css';
 
 class App extends Component {
+
+  static Views() {
+    return {
+      Home: <Home />
+    }
+  }
+
+  static View(props) {
+    let name = props.location.search.substr(1);
+    let view = App.Views()[name];
+    if(view == null)
+      throw new Error("View '" + name + "' is undefined");
+    return view;
+  }
+
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div>
+          <Route path='/' component={App.View}/>
+        </div>
+      </Router>
     );
   }
 }
