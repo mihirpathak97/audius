c3267053caa68c69d06f081069dabca36d42d4ea
Mihir Pathak <mihirpathak97@gmail.com>
2018-05-28 14:49:49 +0530

    add dummy components for about and settings

---
+++ src/views/Settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Settings</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
