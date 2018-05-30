97440f2b474c10f4df1357d84d1547a2067c9fff
Mihir Pathak <mihirpathak97@gmail.com>
2018-05-30 20:03:57 +0530

    Query: display query

--- src/views/Query.js
+++ src/views/Query.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import TopAppBar from '../components/TopAppBar';
 import QueryField from '../components/QueryField';
 
+import { withRouter } from 'react-router-dom';
+
 class Query extends Component {
+
+  state: {
+    query: ''
+  }
+
+  componentWillMount() {
+    this.setState({
+      query: this.props.location.search.split('&')[1].substr(4)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <TopAppBar title="Search" showMenu={false} />
         <TopAppBar title="Search" showMenu={false} showBackIcon={true} />
+        <h1>You searched for "{this.state.query}"</h1>
       </div>
     );
   }
 }
 
-export default Query;
+export default withRouter(Query);
