d0e1411e0ffc7e79b4ae72de293eaa1ddcefa70c
Mihir Pathak <mihirpathak97@gmail.com>
2018-05-31 15:34:20 +0530

    YTCore: init custom module for audio streaming

---
+++ src/modules/YTCore.js
@@ -0,0 +1,59 @@
+
+/*
+  YTCore.js: YouTube audio stream and download
+*/
+
+const path = require('path')
+const express = require('express')
+const nofavicon = require('express-no-favicons')
+const app = express()
+
+const ytdl = require('ytdl-core')
+const through2 = require('through2')
+const Ffmpeg = require('fluent-ffmpeg')
+
+function stream(id, res) {
+  const video = ytdl(id)
+  const ffmpeg = new Ffmpeg(video)
+  const stream = through2()
+
+  try {
+    ffmpeg.format('mp3').pipe(stream)
+    return stream
+  } catch (e) {
+    throw e
+  }
+}
+
+function listen (port, callback = () => {}) {
+
+  // No favicon
+  app.use(nofavicon())
+
+  // display test page
+  app.get('/', (req, res) => {
+    const file = path.resolve(__dirname, 'audio.html')
+    res.sendFile(file)
+  })
+
+  app.get('/:videoId', (req, res) => {
+    const videoId = req.params.videoId
+
+    try {
+      stream(videoId).pipe(res)
+    } catch (e) {
+      console.error(e)
+      res.sendStatus(500, e)
+    }
+  })
+
+  app.use((req, res) => {
+    res.sendStatus(404)
+  })
+
+  app.listen(port, callback)
+}
+
+module.exports = {
+  listen
+}
