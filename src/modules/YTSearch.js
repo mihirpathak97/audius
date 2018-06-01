189f485fdb5c3f34146ae9c1aefb3f725f9a5035
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-01 19:12:24 +0530

    YTSearch: complete minimalised search function

--- src/modules/YTSearch.js
+++ src/modules/YTSearch.js
@@ -8,24 +8,28 @@ var xhr = require('xhr')
 
 if (!xhr.open) xhr = require('request')
 
-var params = {
-  q: 'alan walker faded',
-  part: 'snippet',
-  key: 'AIzaSyBVqWn_4aUZnAtJXSTyg-WRevZrRK3ctPE',
-  maxResults: 10
-}
+module.exports = function search(query, callback) {
 
-xhr({
-  url: 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(params),
-  method: 'GET'
-}, function (err, res, body) {
-  if (err) return err
-
-  try {
-    var result = JSON.parse(body)
-
-    console.log(result);
-  } catch (e) {
-    return e
+  var apiParams = {
+    q: query,
+    key: 'AIzaSyBVqWn_4aUZnAtJXSTyg-WRevZrRK3ctPE',
+    maxResults: 10,
+    part: 'snippet'
   }
-})
+
+  xhr({
+    url: 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(apiParams),
+    method: 'GET'
+  }, function (err, res, body) {
+    if (err) {
+      return callback(err)
+    }
+    try {
+      var result = JSON.parse(body)
+      return callback(null, result)
+    } catch (error) {
+      return callback(error)
+    }
+  })
+
+}
