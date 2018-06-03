f4b80c32941c4c8d625c60f3b9ea1e451ff9b8bd
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-03 11:27:42 +0530

    About: add basic content

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import TopAppBar from '../components/TopAppBar';
 
@@ -7,6 +8,20 @@ class Home extends Component {
     return (
       <div className="App">
         <TopAppBar title="About" showMenu={false} />
+        <div style={{disply: 'block', width: '90%', margin: 'auto', marginTop: 100, textAlign: 'left'}}>
+          <Typography variant="display1" style={{color: 'hsl(348, 100%, 61%)', fontSize: '18', marginBottom: 10}}>
+            About The App
+          </Typography>
+          <Typography variant="heading" style={{fontWeight: 400, fontSize: 18}} gutterBottom>
+            Audius is a cross-platform app that allows you to download songs from YouTube by provviding either
+            the song name, or Spotify/YouTube link.
+          </Typography>
+
+          <Typography variant="display1" style={{fontSize: 16, marginTop: 80}}>
+            Copyright (c) 2018 Mihir Pathak All Rights Reserved.
+          </Typography>
+
+        </div>
       </div>
     );
   }
