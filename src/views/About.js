cc8399bdc34847ea39816f93d11de1f0498d9176
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-03 12:04:46 +0530

    About: clean up

--- src/views/About.js
+++ src/views/About.js
@@ -8,7 +8,7 @@ class Home extends Component {
     return (
       <div className="App">
         <TopAppBar title="About" showMenu={false} />
-        <div style={{disply: 'block', width: '90%', margin: 'auto', marginTop: 100, textAlign: 'left'}}>
+        <div style={{disply: 'block', width: '87%', margin: 'auto', marginTop: 100, textAlign: 'left'}}>
           <Typography variant="display1" style={{color: 'hsl(348, 100%, 61%)', fontSize: '18', marginBottom: 10}}>
             About The App
           </Typography>
@@ -16,11 +16,6 @@ class Home extends Component {
             Audius is a cross-platform app that allows you to download songs from YouTube by provviding either
             the song name, or Spotify/YouTube link.
           </Typography>
-
-          <Typography variant="display1" style={{fontSize: 16, marginTop: 80}}>
-            Copyright (c) 2018 Mihir Pathak All Rights Reserved.
-          </Typography>
-
         </div>
       </div>
     );
