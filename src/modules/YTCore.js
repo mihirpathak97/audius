c828aa987064cf2467ef67f435cdfabbe902f3d5
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-03 18:32:17 +0530

    YTCore: write simple downloadMp3()

--- src/modules/YTCore.js
+++ src/modules/YTCore.js
@@ -3,14 +3,22 @@
   YTCore.js: YouTube audio stream and download
 */
 
-const path = require('path')
-const express = require('express')
-const nofavicon = require('express-no-favicons')
-const app = express()
+const path = require('path');
+const ytdl = require('ytdl-core');
+const through2 = require('through2');
+const Ffmpeg = require('fluent-ffmpeg');
 
-const ytdl = require('ytdl-core')
-const through2 = require('through2')
-const Ffmpeg = require('fluent-ffmpeg')
+var os = require("os");
+var util = require("util");
+var EventEmitter = require("events").EventEmitter;
+var async = require("async");
+var progress = require("progress-stream");
+var sanitize = require("sanitize-filename");
+
+
+const express = require('express');
+const nofavicon = require('express-no-favicons');
+const app = express();
 
 function stream(id, res) {
   const video = ytdl(id)
@@ -54,6 +62,51 @@ function listen (port, callback = () => {}) {
   app.listen(port, callback)
 }
 
+function downloadMp3(youtubeUrl, fileName, callback) {
+
+  const settings = window.require('electron-settings');
+
+  const infoOptions = {
+    quality: 'highest'
+  }
+
+  const downloadOptions = {
+    quality: 'highest',
+    requestOptions: { maxRedirects: 5 }
+  }
+
+  fileName = path.join(settings.get('USERHOME'), fileName, '.mp3');
+
+  ytdl.getInfo(youtubeUrl, infoOptions, function(err, info) {
+    // Setup stream
+    var stream = ytdl.downloadFromInfo(info, downloadOptions);
+    stream.on("response", function(httpResponse) {
+      console.log(httpResponse);
+
+      var str = progress({
+        length: parseInt(httpResponse.headers["content-length"])
+      });
+
+      // Start encoding
+      var proc = new Ffmpeg({
+        source: stream
+      })
+      .audioBitrate(info.formats[0].audioBitrate)
+      .withAudioCodec("libmp3lame")
+      .toFormat("mp3")
+      .on("error", function(err) {
+        callback(err.message);
+      })
+      .on("end", function() {
+        callback(null, true);
+      })
+      .saveToFile(fileName);
+    });
+  });
+
+}
+
 module.exports = {
-  listen
+  listen,
+  downloadMp3
 }
