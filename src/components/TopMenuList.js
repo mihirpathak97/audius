983e81d0d9c08eace3960817c38f7c83ccd545ac
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-05 15:43:14 +0530

    TopMenu: [FIX] menu list was open when window open was evoked

--- src/components/TopMenuList.js
+++ src/components/TopMenuList.js
@@ -35,6 +35,7 @@ class TopMenuList extends React.Component {
   }
 
   openWindow = (url) => {
+    this.setState({ anchorEl: null });
     const miniWindow = new BrowserWindow({width: 800, height: 600, frame: false});
     miniWindow.setResizable(false);
     miniWindow.loadURL(process.env.NODE_ENV == 'development' ? 'http://localhost:3000?' + url : `file://${path.join(app.getAppPath(), 'react-compiled/index.html?' + url)}`);
