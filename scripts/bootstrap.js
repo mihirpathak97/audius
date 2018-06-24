dc6f6362fe8b8647c215c9e31c0d940d24dbff6b
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-24 14:13:34 +0530

    bootstrap: download all platforms

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -7,33 +7,44 @@ var download = require('download-file');
 var admZip = require('adm-zip');
 var path = require('path');
 
-// Download binraries
-const fileNameFFmpeg = process.platform === "win32" ? 'ffmpeg.exe' : 'ffmpeg';
-const fileNameRainbow = process.platform + '.zip';
+function downloadBinaries(platform) {
+    console.log("Downloading binaries for " + platform);
+    const fileNameFFmpeg = platform === "win32" ? 'ffmpeg.exe' : 'ffmpeg';
+    const fileNameRainbow = platform + '.zip';
 
-var urlFFmpeg = "https://s3.ap-south-1.amazonaws.com/mihirpathak/audius/binaries/ffmpeg/" + process.platform + "/" + fileNameFFmpeg;
-var urlRainbow = "https://s3.ap-south-1.amazonaws.com/mihirpathak/audius/binaries/rainbow/" + fileNameRainbow;
- 
-download(urlFFmpeg, {
-    directory: "bin/ffmpeg/" + process.platform,
-    filename: fileNameFFmpeg
-}, function(err){
-    if (err) {
-      console.log(err);
-    }
-    console.log("Finished downloading FFmpeg")
-});
+    var urlFFmpeg = "https://s3.ap-south-1.amazonaws.com/mihirpathak/audius/binaries/ffmpeg/" + platform + "/" + fileNameFFmpeg;
+    var urlRainbow = "https://s3.ap-south-1.amazonaws.com/mihirpathak/audius/binaries/rainbow/" + fileNameRainbow;
 
-download(urlRainbow, {
-    directory: "bin/rainbow/",
-    filename: fileNameRainbow
-}, function(err){
-    if (err) {
-      console.log(err);
-    }
-    console.log("Finished downloading Rainbow")
-    var zip = new admZip(path.join('../bin/rainbow/', fileNameRainbow));
-    console.log("Extracting files...");
-    zip.extractAllTo("../bin/rainbow/" + process.platform, true);
-});
+    download(urlFFmpeg, {
+        directory: "bin/ffmpeg/" + platform,
+        filename: fileNameFFmpeg
+    }, function(err){
+        if (err) {
+          console.log(err);
+        }
+        console.log("Finished downloading FFmpeg for " + platform)
+    });
+    
+    download(urlRainbow, {
+        directory: "bin/rainbow/",
+        filename: fileNameRainbow
+    }, function(err){
+        if (err) {
+          console.log(err);
+        }
+        console.log("Finished downloading Rainbow for " + platform)
+        var zip = new admZip(path.join('bin/rainbow/', fileNameRainbow));
+        console.log("Extracting files...");
+        zip.extractAllTo("bin/rainbow/" + platform, true);
+    });
+}
+
+
+// Download for all platforms [since CI will be doing cross-platform build]
+// You can commnet out the rest of them.
+// 
+// Downloads approximately 150mb worth files.
+downloadBinaries("win32");
+downloadBinaries("linux");
+downloadBinaries("darwin");
 
