0d497ef74feb9275cc969d13db63b78d00832d1c
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-29 12:09:02 +0530

    Settings: enable embedMetadata by default

--- src/modules/Settings.js
+++ src/modules/Settings.js
@@ -26,4 +26,4 @@ settings.has('downloadDirectory') ? null : settings.set('downloadDirectory', pat
 // Default download format
 settings.has('defaultAudioOut') ? null : settings.set('defaultAudioOut', 'mp3');
 // Embed meta-data
-settings.has('embedMetadata') ? null : settings.set('embedMetadata', false);
+settings.has('embedMetadata') ? null : settings.set('embedMetadata', true);
