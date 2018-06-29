45ddb918a0bab43f01cb20f9305079545667b4bb
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-29 12:20:16 +0530

    Settings: use m4a as default in Mac

--- src/modules/Settings.js
+++ src/modules/Settings.js
@@ -10,20 +10,38 @@ const isDev = require('electron-is-dev');
 const path = require('path');
 const settings = require('electron-settings');
 
-// FFmpeg [platform dependant]
 let ffmpegPath = isDev ? path.join(__dirname, '../../bin/ffmpeg', process.platform, 'ffmpeg') : path.join(process.resourcesPath, 'bin', 'ffmpeg', process.platform, 'ffmpeg');
-if (process.platform === 'win32') {
-  ffmpegPath += '.exe';
-}
-settings.set('FFMPEG_PATH', ffmpegPath);
 let rainbowPath = isDev ? path.join(__dirname, '../../bin/rainbow', process.platform, 'rainbow') : path.join(process.resourcesPath, 'bin', 'rainbow', process.platform, 'rainbow');
-if (process.platform === 'win32') {
-  rainbowPath += '.exe';
+let downloadDirectory = path.join(process.env['HOME'], 'Music');
+let defaultAudioOut = "mp3";
+let embedMetadata = true;
+
+if(process.platform === "win32") {
+  // Add exe for binaries in win32
+  ffmpegPath += ".exe";
+  rainbowPath += ".exe";
+  // Use USERPROFILE instead of HOME
+  downloadDirectory = path.join(process.env['USERPROFILE'], 'Music');
 }
+
+if(process.platform === "darwin") {
+  // Default audio out for Mac is M4A
+  defaultAudioOut = "m4a";
+}
+
+// set binary paths
+settings.set('FFMPEG_PATH', ffmpegPath);
 settings.set('RAINBOW_PATH', rainbowPath);
+
 // Default download directory
-settings.has('downloadDirectory') ? null : settings.set('downloadDirectory', path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], 'Music'));
+if(!settings.has('downloadDirectory')) { 
+  settings.set('downloadDirectory', downloadDirectory);
+}
 // Default download format
-settings.has('defaultAudioOut') ? null : settings.set('defaultAudioOut', 'mp3');
+if(!settings.has('defaultAudioOut')) { 
+  settings.set('defaultAudioOut', defaultAudioOut);
+}
 // Embed meta-data
-settings.has('embedMetadata') ? null : settings.set('embedMetadata', true);
+if(!settings.has('embedMetadata')) { 
+  settings.set('embedMetadata', embedMetadata);
+}
