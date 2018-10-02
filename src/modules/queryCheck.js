2a8860b8ca8a0ee3d13bc7ce0747676dc047e212
Mihir Pathak <mihirpathak97@gmail.com>
2018-10-02 12:43:37 +0530

    [Query] Add error for YT playlist links

--- src/modules/queryCheck.js
+++ src/modules/queryCheck.js
@@ -3,6 +3,12 @@ const YTSearch = require('./YTSearch')
 let queryCheck = function (query) {
   return new Promise(function(resolve, reject) {
     if (query.includes('youtube.com')) {
+      if (query.includes('playlist?')) {
+        reject({
+          code: 400,
+          message: 'Playlists are not supported!'
+        })
+      }
       YTSearch.searchVideoById(query.split('?v=')[1]).then(response => {
         // Get Spotify metadata
         Spotify.searchTrackByQuery(response[0].title).then(metadata => {
