aa0cd559d5094ac74f67d24734141b90da97bf04
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-20 20:49:24 +0530

    Do not test for REACT_APP prefix in env vars

--- config/env.js
+++ config/env.js
@@ -58,11 +58,10 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-const REACT_APP = /^REACT_APP_/i;
+// const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
