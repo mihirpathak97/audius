2cc6a00a88d5fe02258feeeb5a0eb1a54b64d3f7
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-20 19:53:02 +0530

    Move to SASS

--- src/app.renderer.js
+++ src/app.renderer.js
@@ -15,7 +15,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
 
 // import global CSS file
-import './app.global.css';
+import './app.global.scss';
 
 // Routes
 import Routes from './routes/';
