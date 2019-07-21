451d43cefb39357d8dc2d845396a6ceaf5b8b7b5
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-07-21 08:20:37 +0530

    Move about page to antd

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import {
-  Typography,
   GridList,
   GridListTile
 } from '@material-ui/core';
 
+import {
+  Typography
+} from 'antd';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faNodeJs, faChrome, faReact } from '@fortawesome/free-brands-svg-icons';
-import electron from './electron-logo.svg';
+import { 
+  faGithub,
+  faNodeJs,
+  faChrome,
+  faReact 
+} from '@fortawesome/free-brands-svg-icons';
+
+import electron from '@/assets/electron-logo.svg';
+
 function openExternal(url) {
   var shell = window.require('electron').shell;
   shell.openExternal(url);
@@ -17,54 +27,50 @@ class View extends Component {
   render() {
     return (
       <div>
-        <div style={{disply: 'block', width: '87%', margin: 'auto', marginTop: 10, textAlign: 'left'}}>
-          <Typography variant="display1" style={{color: 'hsl(348, 100%, 61%)', fontSize: '18', marginBottom: 10}}>
+        <div className="about">
+          <Typography.Title className="title">
             About
-          </Typography>
-          <Typography variant="heading" style={{fontWeight: 400, fontSize: 18}} gutterBottom>
+          </Typography.Title>
+          <Typography.Text>
             Audius is a cross-platform app that allows you to download songs from YouTube by providing either
             the song name, or Spotify/YouTube link.
-          </Typography>
-          <Typography variant="title" style={{marginTop: 30, fontWeight: 400, fontSize: 18}} gutterBottom>
-            Image assets courtesy of <span onClick={() => openExternal("http://github.com/turnerboy")} style={{color: 'blue', cursor: 'pointer'}}>@turnerboy</span>
-          </Typography>
-          <Typography variant="subheading" style={{marginTop: 40}} gutterBottom>
+          </Typography.Text>
+          <Typography.Text>
+            Image assets courtesy of <span onClick={() => openExternal("http://github.com/turnerboy")} className="mention">@turnerboy</span>
+          </Typography.Text>
+          <Typography.Title>
             Version {window.require('electron').remote.app.getVersion()}
-          </Typography>
-          <div style={{textAlign: 'center', marginTop: 40}}>
-            <Typography style={{display: 'inline-block'}} variant="title">Built with</Typography>
-            <img alt="Electron Logo" src={electron} style={{width: '150px', display: 'inline-block', marginLeft: '10px'}} />
+          </Typography.Title>
+          <div className="electron">
+            <Typography.Text>Built with</Typography.Text>
+            <img alt="Electron Logo" src={electron}/>
           </div>
-          <GridList style={{marginTop: 60, textAlign: 'center'}} cols={4}>
-            <GridListTile>
+          <div className="icons">
+            <div>
               <FontAwesomeIcon
                 icon={faGithub}
-                onClick={() => openExternal('https://github.com/mihirpathak97/audius')}
-                style={{ fontSize: '30px', cursor: 'pointer' }} />
-              <Typography style={{marginTop: '5px'}}>GitHub</Typography>
-            </GridListTile>
-            <GridListTile>
+              />
+              <Typography onClick={() => openExternal('https://github.com/mihirpathak97/audius')}>GitHub</Typography>
+            </div>
+            <div>
               <FontAwesomeIcon
                 icon={faNodeJs}
-                onClick={() => openExternal('https://nodejs.org')}
-                style={{ fontSize: '30px', cursor: 'pointer' }} />
-              <Typography style={{marginTop: '5px'}}>v{process.versions.node}</Typography>
-            </GridListTile>
-            <GridListTile>
+              />
+              <Typography onClick={() => openExternal('https://nodejs.org')}>v{process.versions.node}</Typography>
+            </div>
+            <div>
               <FontAwesomeIcon
                 icon={faChrome}
-                onClick={() => openExternal('https://electronjs.org')}
-                style={{ fontSize: '30px', cursor: 'pointer' }}/>
-              <Typography style={{marginTop: '5px'}}>v{process.versions.chrome}</Typography>
-            </GridListTile>
-            <GridListTile>
+              />
+              <Typography onClick={() => openExternal('https://electronjs.org')}>v{process.versions.chrome}</Typography>
+            </div>
+            <div>
               <FontAwesomeIcon
                 icon={faReact}
-                onClick={() => openExternal('https://reactjs.org')}
-                style={{ fontSize: '30px', cursor: 'pointer' }} />
-              <Typography style={{marginTop: '5px'}}>v{React.version}</Typography>
-            </GridListTile>
-          </GridList>
+              />
+              <Typography onClick={() => openExternal('https://reactjs.org')}>v{React.version}</Typography>
+            </div>
+          </div>
         </div>
       </div>
     );
