9eff6c4dd8eb8843c065b04d749e7c1a2d13a078
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-09-22 14:52:55 +0530

    Set download queue as array

--- src/reducers/downloadQueue.js
+++ src/reducers/downloadQueue.js
@@ -1,22 +1,16 @@
 import { ADD_TO_QUEUE, REMOVE_FROM_QUEUE } from '../actions/downloadQueue';
 
-const initialState = {
-  queue: []
-}
+const initialState = [];
 
 export default function downloadQueue(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_QUEUE:
-      return {
-        queue: [...state.queue, action.queueItem]
-      }
+      return [...state.queue, action.queueItem];
     case REMOVE_FROM_QUEUE:
-      return {
-        queue: [
-          ...state.queue.slice(0, action.index),
-          ...state.queue.slice(action.index + 1)
-        ]
-      }
+      return [
+        ...state.queue.slice(0, action.index),
+        ...state.queue.slice(action.index + 1)
+      ];
     default:
       return state;
   }
